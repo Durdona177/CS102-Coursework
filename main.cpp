68e732e57a9cdc3914abff16faefffa22637d078
250042 <dsayfiddinova58@gmail.com>
2026-01-27 12:35:44 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "Hello World" << endl;
+    cout << "Bye\n";
+
+    cout << "Name: Durdona" << endl;
+    cout << "Major: Education";
+
+    return 0;
+}
+
